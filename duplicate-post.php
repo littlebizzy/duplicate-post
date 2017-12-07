c1406736f08bf9fe55b808b917ec49cae7d66d09
Jesse Nickles <jesse@littlebizzy.com>
2017-12-06 18:04:56 -0800

    Add files via upload

---
+++ duplicate-post.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Plugin Name: Duplicate Post
+Plugin URI: https://www.littlebizzy.com/plugins/duplicate-post
+Description: Easily duplicate (clone) any blog post, custom post, or page in WordPress which are then saved in Draft mode, saving you tons of time and headache.
+Version: 1.0.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+Text Domain: duplicate-post-littlebizzy
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+Domain Path: /lang
+*/
+
+
+/**
+ * Define main plugin class
+ */
+class LB_Duplicate_Post {
+
+	/**
+	 * A reference to an instance of this class.
+	 *
+	 * @since 1.0.0
+	 * @var   object
+	 */
+	private static $instance = null;
+
+	/**
+	 * Initalize plugin actions
+	 *
+	 * @return void
+	 */
+	public function init() {
+
+		// This plugin is nothing to do on frontend.
+		if ( ! is_admin() ) {
+			return;
+		}
+
+		$this->lang();
+
+		include $this->dir() . 'includes/class-lb-duplicate-post-handler.php';
+
+		lb_duplicate_post_handler()->init();
+	}
+
+	/**
+	 * Loads the translation files.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function lang() {
+		load_plugin_textdomain( 'duplicate-post-littlebizzy', false, basename( dirname( __FILE__ ) ) . '/lang' );
+	}
+
+	/**
+	 * Returns plugin base file
+	 *
+	 * @return string
+	 */
+	public static function file() {
+		return __FILE__;
+	}
+
+	/**
+	 * Returns plugin base file
+	 *
+	 * @return string
+	 */
+	public function dir() {
+		return trailingslashit( dirname( __FILE__ ) );
+	}
+
+	/**
+	 * Returns the instance.
+	 *
+	 * @since  1.0.0
+	 * @return object
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+}
+
+/**
+ * Returns instance of LB_Duplicate_Post class
+ *
+ * @return object
+ */
+function lb_duplicate_post() {
+	return LB_Duplicate_Post::get_instance();
+}
+
+/**
+ * Initalize plugin instance very on 'init' hook
+ */
+add_action( 'init', array( lb_duplicate_post(), 'init' ), 20 );
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Duplicate Post ===
+
+Contributors: LittleBizzy
+Tags: duplicate post, duplicate, clone, copy, post, page, custom post
+Requires at least: 4.4
+Tested up to: 4.8
+Stable tag: 1.0.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Easily duplicate (clone) any blog post, custom post, or page in WordPress which are then saved in Draft mode, saving you tons of time and headache.
+
+== Description ==
+
+Easily duplicate (clone) any blog post, custom post, or page in WordPress which are then saved in Draft mode, saving you tons of time and headache.
+
+Zero settings page to worry about, simply adds a "Duplicate" link to the post listings and page listings area.
+
+== Installation ==
+
+1. Upload to `/wp-content/plugins/` directory
+2. Activate the plugin in WP Admin
+
+== Changelog ==
+
+= 1.0.0 =
+* initial release
