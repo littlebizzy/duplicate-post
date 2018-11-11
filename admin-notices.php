83a9ed4002cecd7477e2a800caee7a12f9d4658c
Jesse Nickles <jesse@littlebizzy.com>
2018-11-10 16:37:17 -0800

    Update admin-notices.php

--- admin-notices.php
+++ admin-notices.php
@@ -24,7 +24,7 @@ final class DPLCTP_Admin_Notices {
 	private $rate_us_url2 = 'https://www.facebook.com/groups/littlebizzy/';
 	private $rate_us_url3 = 'https://www.littlebizzy.com/services/speed-boost?utm_source=wpnotice';
 	private $rate_us_url4 = 'https://codex.wordpress.org/Plugin_API/Action_Reference/admin_notices#Disable_Nag_Notices';
-	private $rate_us_message = 'Howdy, friend :)<br><br>Thanks for using <strong>%plugin%</strong>. Please support our free work by rating this plugin with 5 stars on <a href="%url%" target="_blank">WordPress.org</a>.<br><br>You may also join our free <a href="%url2%" target="_blank">Facebook group</a> to post any questions or comments!<br><br>Cheers, Jesse<br><br>P.S. Use coupon code <code>SPEED20</code> to get $20 off our popular <a href="%url3%" target="_blank">Speed Boost</a> service at LittleBizzy.com.<br><br><small><em><a href="%url4%" target="_blank">Hide these notices</a></em></small>';
+	private $rate_us_message = 'Thanks for using <strong>%plugin%</strong>. Please support our free work by rating this plugin with 5 stars on <a href="%url%" target="_blank">WordPress.org</a>.<br><br>You may also join our free <a href="%url2%" target="_blank">Facebook group</a> to post any questions or comments!<br><br>Cheers, Jesse<br><br>P.S. Use coupon code <code>SPEED20</code> to get $20 off our popular <a href="%url3%" target="_blank">Speed Boost</a> service at LittleBizzy.com.<br><br><small><em><a href="%url4%" target="_blank">Hide these notices</a></em></small>';
 
 
 
@@ -34,10 +34,10 @@ final class DPLCTP_Admin_Notices {
 	private $days_dismissing_suggestions = 180; // 6 months reappear
 	private $suggestions_message = '%plugin% recommends the following free plugins:';
 	private $suggestions = array(
-		'disable-empty-trash-littlebizzy' => array(
-			'name' => 'Disable Empty Trash',
-			'desc' => 'Completely disables the automatic trash empty for WordPress posts, custom posts, pages, and comments to avoid data loss and encourage manual emptying.',
-			'filename' => 'disable-empty-trash.php',
+		'maintenance-mode-littlebizzy' => array(
+			'name' => 'Maintenance Mode',
+			'desc' => 'Minimalistic plugin with a simple one-click on/off switch and zero settings to worry about, featuring default WordPress styling without any images.',
+			'filename' => 'maintenance-mode.php',
 		),
 		'404-to-homepage-littlebizzy' => array(
 			'name' => '404 To Homepage',
@@ -54,10 +54,10 @@ final class DPLCTP_Admin_Notices {
 			'desc' => 'Useful statistics about the server OS, CPU, RAM, load average, memory usage, IP address, hostname, timezone, disk space, PHP, MySQL, caches, etc.',
 			'filename' => 'server-status.php',
 		),
-		'maintenance-mode-littlebizzy' => array(
-			'name' => 'Maintenance Mode',
-			'desc' => 'Minimalistic plugin with a simple one-click on/off switch and zero settings to worry about, featuring default WordPress styling without any images.',
-			'filename' => 'maintenance-mode.php',
+		'disable-empty-trash-littlebizzy' => array(
+			'name' => 'Disable Empty Trash',
+			'desc' => 'Completely disables the automatic trash empty for WordPress posts, custom posts, pages, and comments to avoid data loss and encourage manual emptying.',
+			'filename' => 'disable-empty-trash.php',
 		),
 	);
 
